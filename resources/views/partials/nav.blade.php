4e658c2c6deb05cc60a5842122cae660b7174247
Kate <murraykeaton422@gmail.com>
2023-12-21 00:32:13 -0500

    declared route in nav link to display contact form when clicked

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -11,7 +11,7 @@
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/">Home</a></li>
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/about-us">About</a></li>
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Create Post</a></li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/contact-us">Contact</a></li>
                 </ul>
             </div>
         </div>
