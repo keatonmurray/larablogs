bf57a4492007bce0a25be8a2de3632a7e706506c
Kate <murraykeaton422@gmail.com>
2023-12-22 01:22:39 -0500

    enabled auth scaffolding using laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ViewController;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,15 @@ Route::get('/about-us', [PagesController::class, 'about']);
 Route::get('/contact-us', [PagesController::class, 'contact']);
 Route::get('/edit-post/{id}', [ViewController::class, 'edit']);
 Route::post('/store', [ViewController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
