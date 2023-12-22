98153318e4ac7936a537402c20bbc7e63a42ec04
Kate <murraykeaton422@gmail.com>
2023-12-21 21:39:50 -0500

    image display works

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -19,7 +19,14 @@
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
-                    {!!$blogs->body!!}                   
+                    {!!$blogs->body!!}
+                    <a href="#!"><img class="img-fluid" src="{{asset('storage/' . $blogs->image)}}" /></a>
+                        <p>    
+                            Placeholder text by
+                            <a href="http://spaceipsum.com/">Space Ipsum</a>
+                            &middot; Images by
+                            <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>
+                        </p>                   
                 </div>
             </div>
         </div>
