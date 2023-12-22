1bd326986ca65eec5a2015069f3538001a8d055f
Kate <murraykeaton422@gmail.com>
2023-12-21 21:11:37 -0500

    added new column to blogs table for adding images

--- database/migrations/2023_12_20_133555_create_blogs_table.php
+++ database/migrations/2023_12_20_133555_create_blogs_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('subtext');
             $table->string('author');
+            $table->string('image');
             $table->longText('body');
             $table->timestamps();
         });
