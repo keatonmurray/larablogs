c3a19b7b7736e1b04c1c8d6d288eb95135d19d8e
Kate <murraykeaton422@gmail.com>
2023-12-23 08:25:50 -0500

    created exceptions in middleware so that the index page can be viewed by the public

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -9,7 +9,7 @@ class ViewController extends Controller
 {
 
     public function __construct() {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['index', 'show']]);
     }
     
     public function index() {
