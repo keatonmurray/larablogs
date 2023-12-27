76ba84507282962da1ac9e3b23bf6b6315dd5816
Kate <murraykeaton422@gmail.com>
2023-12-27 09:32:10 -0500

    re-migrated tables and removed the subtext column

--- database/migrations/2023_12_20_133555_create_blogs_table.php
+++ database/migrations/2023_12_20_133555_create_blogs_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('blogs', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('subtext');
             $table->string('image');
             $table->longText('body');
             $table->timestamps();
