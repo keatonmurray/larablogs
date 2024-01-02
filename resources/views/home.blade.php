cc41f99d8d99fa7f1aab2634162b3ee66426b732
Kate <murraykeaton422@gmail.com>
2024-01-02 03:26:16 -0500

    completed dashboard frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,49 @@
 @section('content')
 <header class="masthead" style="background-image:url('{{asset('assets/img/post-bg.jpg')}}')">
     <x-header-container>
-        @foreach($blogs as $blog)
-            <h1>Welcome</h1>  
-            <span class="subheading">Great to have you around, {{$blog->user->name}}!</span>
-        @endforeach  
+        <h1>Welcome</h1>  
+        <span class="subheading">Great to have you around! Feel free to edit, remove, or create new blogs using the navigation below.</span>
     </x-header-container>
 </header>
+<div class="container py-4">
+    <div class="text-center">
+        <a href="/create-post" class="btn btn-primary">Create New Post</a>
+        <a href="/" class="btn btn-primary">See More</a>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-body">
+                    <div class="jumbotron text-center">
+                        <h3>Your Blog Posts</h3>
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Post Title</th>
+                                <th>Action</th>
+                            </tr>
+                            @if(count($blogs)  > 0)
+                                @foreach($blogs as $blog)
+                                    <tr>
+                                        <td>{{$blog->title}}</td>
+                                        <td>
+                                            <form action="" method="POST">
+                                                @csrf
+                                                <a href="" class="btn btn-secondary">Edit</a>
+                                                <button class="btn btn-danger" type="submit">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                @else
+                                <p>No have currently not posted anything</p>
+                            @endif
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
