13f445925ed12322ea74d73b80cd8fb663d3e31e
Kate <murraykeaton422@gmail.com>
2024-01-03 01:58:50 -0500

    locations page completed

--- resources/views/pages/contents/location.blade.php
+++ resources/views/pages/contents/location.blade.php
@@ -1,19 +1,47 @@
 @extends('layouts.app')
 @section('content')
-    <header class="masthead" style="background-image:url('{{asset('assets/img/about-bg.jpg')}}')">
+    <header class="masthead" style="background-image:url('{{asset('assets/img/map.jpg')}}')">
         <x-header-container>
-            <h1>About Me</h1>
-            <span class="subheading">This is what I do.</span>
+            <h1>Visit Us</h1>
+            <span class="subheading">We operate across 2 different cities. Check our locations below.</span>
         </x-header-container>
     </header>
     <!-- Main Content-->
     <main class="mb-4">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                        <div class="card-body">
+                        <div class="jumbotron text-center">
+                            <h3>Our Locations</h3>
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>City</th>
+                                    <th>Street Address</th>
+                                    <th>Contact #</th>
+                                </tr>
+                                <div>
+                                    <tr>
+                                        <td>
+                                            <a href="" class="fw-bold text-decoration-none text-black">Ontario, CA</a>
+                                            <br>
+                                            <a href="" class="fw-bold text-decoration-none text-black">Halifax, CA</a>
+                                        </td>
+                                        <td>
+                                            <a href="" class="fw-bold text-decoration-none text-black">123 Connecticut Ave</a>
+                                            <br>
+                                            <a href="" class="fw-bold text-decoration-none text-black">41 Fox Blvd</a>
+                                        </td>
+                                        <td>
+                                            <a href="" class="fw-bold text-decoration-none text-black">241-111-0012</a>
+                                           <br>
+                                            <a href="" class="fw-bold text-decoration-none text-black">998-009-5423</a>
+                                        </td>
+                                    </tr>
+                                </div>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
