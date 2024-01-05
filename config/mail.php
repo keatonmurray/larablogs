2f76a948e8d929ef6d0cee436ed406f7d1a0f647
Kate <murraykeaton422@gmail.com>
2024-01-05 02:58:38 -0500

    modified env settings for password reset

--- config/mail.php
+++ config/mail.php
@@ -100,7 +100,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'murraykeaton422@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
