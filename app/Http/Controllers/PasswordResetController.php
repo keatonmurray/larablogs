2528363a1fb6379cbb813ab86a08a9a3b1f6783b
Kate <murraykeaton422@gmail.com>
2024-01-05 02:03:56 -0500

    created forgotPassword method which sends a reset password link to the designated user's email address, and resetPassword method for resetting the password

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class PasswordResetController extends Controller
 {
-    //
+    public function forgotPassword() {
+
+    }
+
+    public function reserPassword() {
+        
+    }
 }
