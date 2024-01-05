9210b8091f81eac0c7839f0eaa58950557d1460f
Kate <murraykeaton422@gmail.com>
2024-01-05 06:24:36 -0500

    updated bootstrap classes for responsiveness

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -25,7 +25,7 @@
                                     </div>
                                     <div class="d-flex gap-2 justify-content-center mt-4">
                                         <button class="btn btn-primary text-uppercase" id="submitButton" type="submit">
-                                            {{ __('Send Reset Link') }}
+                                            {{ __('Send Link') }}
                                         </button>
                                         <a href="/" class="btn btn-primary text-uppercase" id="submitButton" type="buttong">
                                             {{ __('Go Back') }}
