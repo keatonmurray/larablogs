399a8449d1df9e87d6e017a3840606ca83ab3c3f
Kate <murraykeaton422@gmail.com>
2024-01-05 02:00:29 -0500

    added bg image to the location page, everything is now completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,51 +6,53 @@
         <span class="subheading">Great to have you around! Feel free to edit, remove, or create new blogs using the navigation below.</span>
     </x-header-container>
 </header>
-<div class="container py-4">
-    <div class="text-center">
-        <a href="/create-post" class="btn btn-primary">Create New Post</a>
-        <a href="/" class="btn btn-primary">Show All</a>
-    </div>
-    <br>
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                    <div class="card-body">
-                    <div class="jumbotron text-center">
-                        <h3>Your Blog Posts</h3>
-                        <table class="table table-striped">
-                            <tr>
-                                <th>Post Title</th>
-                                <th>Action</th>
-                            </tr>
-                            @if(count($blogs)  > 0)
-                                @foreach($blogs as $blog)
-                                    <div class="d-flex">
-                                        <tr>
-                                            <td>
-                                                <a href="/post/{{$blog->id}}" class="fw-bold text-decoration-none text-black">{{$blog->title}}</a></td>
-                                            <td>
-                                                <form action="/delete/{{$blog->id}}" method="POST">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <div class="d-flex gap-2 d-md-flex">
-                                                        <a href="/edit-post/{{$blog->id}}" class="btn btn-secondary">Edit</a>
-                                                        <button class="btn btn-danger" type="submit">Delete</button>
-                                                    </div>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    </div>
-                                @endforeach
-                                @else
-                                <p>You haven't posted anything yet</p>
-                            @endif
-                        </table>
+<main class="mb-4">
+    <div class="container py-4">
+        <div class="text-center">
+            <a href="/create-post" class="btn btn-primary">Create New Post</a>
+            <a href="/" class="btn btn-primary">Show All</a>
+        </div>
+        <br>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+                        <div class="card-body">
+                        <div class="jumbotron text-center">
+                            <h3>Your Blog Posts</h3>
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Post Title</th>
+                                    <th>Action</th>
+                                </tr>
+                                @if(count($blogs)  > 0)
+                                    @foreach($blogs as $blog)
+                                        <div class="d-flex">
+                                            <tr>
+                                                <td>
+                                                    <a href="/post/{{$blog->id}}" class="fw-bold text-decoration-none text-black">{{$blog->title}}</a></td>
+                                                <td>
+                                                    <form action="/delete/{{$blog->id}}" method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <div class="d-flex gap-2 d-md-flex">
+                                                            <a href="/edit-post/{{$blog->id}}" class="btn btn-secondary btn-sm">Edit</a>
+                                                            <button class="btn btn-danger btn-sm" type="submit">Delete</button>
+                                                        </div>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        </div>
+                                    @endforeach
+                                    @else
+                                    <p>You haven't posted anything yet</p>
+                                @endif
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</main>
 @endsection
