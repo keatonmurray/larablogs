b92bb8c8cc5af7f1d0ff7f0deda961f39f05d0a4
Kate <murraykeaton422@gmail.com>
2024-01-05 21:11:09 -0500

    user class implemented mustverifyemail interface and imported class, all users are required to verify their email address

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use App\Models\Blogs;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
